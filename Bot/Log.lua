6a477a1041b358f2034c22c46e9efb1c21436244
Neer <legitting@gmail.com>
2020-05-15 20:53:37 +0200

    Better support

--- Bot/Log.lua
+++ Bot/Log.lua
@@ -3,14 +3,14 @@ DMW.Bot.Log = {}
 local Log = DMW.Bot.Log
 
 
-function Log:NormalInfo(str)
-    DEFAULT_CHAT_FRAME:AddMessage("|TInterface\\Icons\\Ability_DualWield:20:20:0:0:20:20:0:20:0:20|t |cfff54272[Lilium]|r |cff7CFC00[Normal]|r " .. str)
+function Log:NormalInfo(...)
+    DEFAULT_CHAT_FRAME:AddMessage("|TInterface\\Icons\\Ability_DualWield:20:20:0:0:20:20:0:20:0:20|t |cfff54272[Lilium]|r |cff7CFC00[Normal]|r " .. ...)
 end
 
-function Log:DebugInfo(str)
-   DEFAULT_CHAT_FRAME:AddMessage("|TInterface\\Icons\\INV_Misc_Note_01:20:20:0:0:20:20:0:20:0:20|t |cfff54272[Lilium]|r |cffFFA500[Info]|r " .. str)
+function Log:DebugInfo(...)
+   DEFAULT_CHAT_FRAME:AddMessage("|TInterface\\Icons\\INV_Misc_Note_01:20:20:0:0:20:20:0:20:0:20|t |cfff54272[Lilium]|r |cffFFA500[Info]|r " .. ...)
 end
 
-function Log:SevereInfo(str)
-    DEFAULT_CHAT_FRAME:AddMessage("|TInterface\\Icons\\Ability_Creature_Cursed_02:20:20:0:0:20:20:0:20:0:20|t |cfff54272[Lilium]|r |cffFF0000[Error]|r " .. str)
+function Log:SevereInfo(...)
+    DEFAULT_CHAT_FRAME:AddMessage("|TInterface\\Icons\\Ability_Creature_Cursed_02:20:20:0:0:20:20:0:20:0:20|t |cfff54272[Lilium]|r |cffFF0000[Error]|r " .. ...)
 end
